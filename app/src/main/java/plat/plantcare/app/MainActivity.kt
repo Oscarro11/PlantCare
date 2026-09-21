6a3586aa0e7695092199e7ece01577832adf2bdf
Oscarro11 <oscarrodrigomelchore@gmail.com>
2026-09-20 21:09:52 -0600

    fix for initial configuration (missing files)

--- app/src/main/java/plat/plantcare/app/MainActivity.kt
+++ app/src/main/java/plat/plantcare/app/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PlantcareTheme {
-                LoginScreen()
+                Greeting("world")
             }
         }
     }
